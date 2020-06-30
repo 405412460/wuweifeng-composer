50907ca7598c1903df5ea7b94855397d25004362
wuweifeng <405412460@qq.com>
2020-06-30 15:14:28 +0800

    又拍云工具类更新

--- upyun/sdk/src/Upyun/Util.php
+++ upyun/sdk/src/Upyun/Util.php
@@ -87,4 +87,8 @@ class Util
         );
         return strtr(rawurlencode($url), array_merge($reserved, $unescaped, $score));
     }
+    public static function isSuccess($code)
+    {
+        return $code >= 200 && $code < 300;
+    }
 }
